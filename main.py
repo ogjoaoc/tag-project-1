5bb8ba95b31584228fc7a59db84ce97e7866e14e
Ricardo de Carvalho Nabuco <131546157+ricardocnabuco@users.noreply.github.com>
2024-11-23 16:43:11 -0300

    Atualização com as funções de cliques maximais e grafos com cliques destacados

--- main.py
+++ main.py
@@ -50,19 +50,59 @@ def inicializar_lista_adj(G):        # Contrução de uma lista de adjacências 
 
 def visualizar_grafo_com_cliques(grafo, cliques):    # Gerar uma visualizaação do grafo com cliques destacados com cores.
 
-    pos = nx.spring_layout(grafo)
+    pos = nx.spring_layout(grafo)   # Definir um layout para posicionar os nós no grafo. 
     
+    clique_colors = [f"#{random.randint(0, 0xFFFFFF):06x}"
+                    for _ in range(len(cliques))]    # Gerar cores únicas para cada clique maximal.
 
-matriz = mmread("soc-dolphins\\soc-dolphins.mtx")
-grafo = nx.Graph(matriz)
-MAX_NODOS = len(grafo.nodes())
+    colors = {}
+    for i, clique in enumerate(cliques):    # Mapear as cores para os nós, com uma cor padrão para nós fora de cliques.
+        for node in clique:
+            colors[node] = clique
 
-lista_de_adj = inicializar_lista_adj(grafo)
-tabela_graus_por_vertice = grau_por_vertice(lista_de_adj)
+    default_color = "#cccccc"        # Definição cor padrão (cinza) para nós que não pertencem a cliques destacados
+    node_colors = [colors.get(node, default_color) for node in grafo.nodes()]
 
-print(lista_de_adj)
-print(coef_aglomeracao(lista_de_adj, 34))
-print(coef_aglomeracao_medio(lista_de_adj))
-print(nx.average_clustering(grafo))
+    nx.draw(            # Desenho do Grafo.
+        grafo,
+        pos,
+        with_labels=True,
+        node_color=node_colors,
+        edge_color="#999999",
+        node_size=500,
+        font_size=10,
+    )
+    plt.title("Grafo com Cliques Destacados")
+    plt.show()
 
+matriz = mmread("soc-dolphins\\soc-dolphins.mtx")    # Carrega a matriz do arquivo no formato Matrix Market.
+grafo = nx.Graph(matriz)     # Construção do grafo usando NetworkX.
+MAX_NODOS = len(grafo.nodes())    # Definição do número total de nós.
 
+lista_de_adj = inicializar_lista_adj(grafo) # Construção da Lista de adjacências.
+tabela_graus_por_vertice = grau_por_vertice(lista_de_adj)    # Cálculo dos graus de cada vértice.
+
+print(lista_de_adj) # Impressão da lista de Adjacências
+print(coef_aglomeracao(lista_de_adj, 34)) # Impressão do vértice 34 e seu coeficiente de aglomeração.
+print(coef_aglomeracao_medio(lista_de_adj)) # Impressão do coeficiente de aglomeração médio. 
+print(nx.average_clustering(grafo)) # Impressão do coeficiente médio.
+
+def encontrar_cliques_maximais(grafo):    # Função para os cliques maximais
+    """
+    Exibir todos os cliques maximais no grafo.
+    grafo (networkx.Graph): Grafo carregado no NetworkX.
+    list: Lista de cliques maximais, onde cada clique é representado por uma lista de nós.
+    """
+    
+    cliques_maximais = list(nx.find_cliques(grafo))    # Encontrar os cliques maximais usando NetworkX
+
+    print("\nCliques Maximais:") # Exibição dos cliques encontrados.
+
+    for i, clique in enumerate(cliques_maximais, start=1):
+        print(f"Clique {i} ({len(clique)} vértices): {clique}")
+
+    return cliques_maximais
+
+cliques_maximais = encontrar_cliques_maximais(grafo) # Encontrar os cliques maximais no grafo.
+
+visualizar_grafo_com_cliques(grafo, cliques_maximais) # Visualização do grafo com os cliques destacados.
