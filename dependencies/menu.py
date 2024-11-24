7abc8698fd096918febb1f65ed817b4ac5109a46
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2024-11-24 15:24:22 -0300

    refact bron

--- dependencies/menu.py
+++ dependencies/menu.py
@@ -1,7 +1,16 @@
 from main import *
 
+'''
+menu.py
+Projeto 1 - Teoria e Aplicação de Grafos.
+Autores: João Carlos (232009511) e Ricardo Nabuco (231021360).
+Professor: Díbio.
+'''
+
 def inicio():
+
     while True:
+
         print()
         print("Bem-vindo usuário!")
         print()
@@ -10,11 +19,24 @@ def inicio():
         print("(2) Todos os cliques maximais.")
         print("(3) O coeficiente de Aglomeração de todos vértices do grafo.")
         print("(4) O coeficiente médio de Aglomeração.")
-        print("(5) Visualização do grafo completo.")
+        print("(5) Visualização do grafo completo (indexado em 0, vértices : [0, 61]).")
         print("(6) Sair.")
         
-        print("Selecione a opção desejada: ")
-        opcao = int(input())
+        while True:
+
+            print("Selecione a opção desejada: ")
+            
+            try:
+                opcao = int(input())
+                break
+            except:
+                limpar_terminal()
+                print("A opção deve ser um número inteiro válido, de 1 a 6.")
+                print()
+                skip = input("Digite enter para tentar novamente...")
+                limpar_terminal()
+
+            
         limpar_terminal()
 
         match opcao:
@@ -25,7 +47,8 @@ def inicio():
             case 3:
                 print_coef_alomeracao_por_vertice()
             case 4:
-                print(coef_aglomeracao_medio(lista_de_adj))
+                print()
+                print(f'Coeficiente médio de aglomeração: {coef_aglomeracao_medio(lista_de_adj)}')
             case 5:
                 visualizar_grafo_com_cliques(grafo, encontrar_cliques_maximais(grafo)) # Visualização do grafo com os cliques destacados.
             case 6:
--- main.py
+++ main.py
@@ -1,122 +1,140 @@
 import networkx as nx
 import random
 import matplotlib.pyplot as plt
+import sys
 from scipy.io import mmread
 from collections import defaultdict
 from os import system, name
+sys.dont_write_bytecode = True
 
+'''
+main.py
+Projeto 1 - Teoria e Aplicação de Grafos.
+Autores: João Carlos (232009511) e Ricardo Nabuco (231021360).
+Professor: Díbio.
+'''
 
+def bron_kerbosch(C, P, E, cliques):
 
-def print_coef_alomeracao_por_vertice():
+    '''
+    Algoritmo de Bron-Kerborsch:
+    C - Vértices do clique que está sendo feito.
+    P - Candidatos a participar do clique
+    X - Nós que já foram processados.
+    cliques - Lista com os cliques maximais.
+    Fonte: slides da disciplina (Aula 5).
+    '''
+
+    if not P and not E:
+        cliques.append(C)
+        return
+    
+    for candidato in list(P):
+        bron_kerbosch(C.union({candidato}), P.intersection(set(grafo.neighbors(candidato))), E.intersection(set(grafo.neighbors(candidato))), cliques)
+        P.remove(candidato)
+        E.add(candidato)
+
+def print_coef_alomeracao_por_vertice():    # Módulo para impressão dos vértices e seus respectivos coeficientes de aglomeração.
 
     for i in range(1, MAX_NODOS + 1):
         print(f'Vértice: {i} - {coef_aglomeracao(lista_de_adj, i)}')
 
-def coef_aglomeracao(adj, v):         # Calcula o coeficiente de aglomeração para o vértice v.
+def coef_aglomeracao(adj, v):               # Calcula o coeficiente de aglomeração para o vértice v.
     
-    qtd_vizinhos = len(adj[v])        # Número de vizinhos do vértice.
+    qtd_vizinhos = len(adj[v])              # Número de vizinhos do vértice.
 
-    if qtd_vizinhos < 2:              # Se o vértice não tem no mínimo dois vizinhos, não há triângulos. 
+    if qtd_vizinhos < 2:                    # Se o vértice não tem no mínimo dois vizinhos, não há triângulos. 
         return 0.0
     
-    entre_vizinho = 0                 # Contador para conexões entre vizinhos.
+    entre_vizinho = 0                       # Contador para conexões entre vizinhos.
 
-    for x in adj[v]:                  # Loop para verificar pares de vizinhos do vértice v.
+    for x in adj[v]:                        # Loop para verificar pares de vizinhos do vértice v.
         for y in adj[v]:
             if x != y and x in adj[y] and y in adj[v]:
                 entre_vizinho += 1
 
-    return entre_vizinho / ((qtd_vizinhos) * (qtd_vizinhos - 1))           # Retorna o coeficiente de aglomeração.
+    return entre_vizinho / ((qtd_vizinhos) * (qtd_vizinhos - 1))       
 
-def coef_aglomeracao_medio(adj):      # Função para calcular o coeficiente de aglomeração médio do grafo.
+def coef_aglomeracao_medio(adj):            # Função para calcular o coeficiente de aglomeração médio do grafo.
 
-    return sum(coef_aglomeracao(adj, v) for v in range(1, MAX_NODOS + 1)) / MAX_NODOS  # Soma os coeficientes de todos os vértices e divide pelo número total de nós.
+    return sum(coef_aglomeracao(adj, v) for v in range(1, MAX_NODOS + 1)) / MAX_NODOS  
 
-def grau_por_vertice(adj):          # Calcula o grau de cada vértice.
+def grau_por_vertice(adj):                  # Calcula o grau de cada vértice.
     
-    tabela = defaultdict(int)       # Dicionário com o grau de cada vértice. 
+    tabela = defaultdict(int)               # Dicionário com o grau de cada vértice. 
 
-    for nodo in range(1, MAX_NODOS + 1):  # Conta o número de vizinhos de cada vértice.
+    for nodo in range(1, MAX_NODOS + 1):    # Conta o número de vizinhos de cada vértice.
         tabela[nodo] = len(adj[nodo])
         
     return tabela
 
-def inicializar_lista_adj(G):        # Contrução de uma lista de adjacências a partir do grafo.
+def inicializar_lista_adj(G):               # Contrução de uma lista de adjacências a partir do grafo.
     
-    adj = [[] for _ in range(MAX_NODOS + 1)]        # Cria uma lista de adjacências para todos os nós.
+    adj = [[] for _ in range(MAX_NODOS + 1)]        
     
-    for aresta in G.edges():         # Adiciona as conexões de cada aresta à lista de adjacências.
+    for aresta in G.edges():                # Adiciona as conexões de cada aresta à lista de adjacências.
         u, v = aresta[0], aresta[1]
         adj[u + 1].append(v + 1)
         adj[v + 1].append(u + 1)
 
     return adj
 
-def visualizar_grafo_com_cliques(grafo, cliques):    # Gerar uma visualização do grafo com cliques destacados com cores.
+def visualizar_grafo_com_cliques(grafo, cliques):    
 
-    pos = nx.spring_layout(grafo)   # Definir um layout para posicionar os nós no grafo. 
+    pos = nx.spring_layout(grafo)           # Definir um layout para posicionar os nós no grafo. 
     
     clique_colors = [f"#{random.randint(0, 0xFFFFFF):06x}" for _ in range(len(cliques))]    # Gerar cores únicas para cada clique maximal.
 
     colors = {}
 
-    for i in range(len(cliques)):   # Mapear as cores para os nós, com uma cor padrão para nós fora de cliques.
+    for i in range(len(cliques)):           # Mapear as cores para os nós, com uma cor padrão para nós fora de cliques.
         for no in cliques[i]:
             colors[no] = clique_colors[i]
             
-    default_color = "#cccccc"        # Definição cor padrão (cinza) para nós que não pertencem a cliques destacados
+    default_color = "#cccccc"               # Definição cor padrão (cinza) para nós que não pertencem a cliques destacados.
     node_colors = [colors.get(node, default_color) for node in grafo.nodes()]
 
-    nx.draw(            # Desenho do Grafo.
-        grafo,
-        pos,
-        with_labels=True,
-        node_color=node_colors,
-        edge_color="#999999",
-        node_size=500,
-        font_size=10,
-    )
-
-    plt.title("Grafo com Cliques Destacados")
+    nx.draw(grafo, pos, with_labels=True, node_color=node_colors, edge_color="#999999", node_size=500, font_size=10)
+    plt.gcf().canvas.manager.set_window_title("Visualização do Grafo - Cliques maximais destacados por cores") 
     plt.show()
 
-def menu():
-    from menu import inicio  
+def menu():                                 # Módulo para inicializar menu do programa.
+
+    from dependencies.menu import inicio  
     inicio()
 
-def limpar_terminal():
-    if name == 'nt':
+def limpar_terminal():                     # Módulo para flush do terminal.
+
+    if name == 'nt':                       # Alterna entre diferentes sistemas operacionais.
         _ = system('cls')
     else:
         _ = system('clear')
 
-def encontrar_cliques_maximais(grafo):    # Função para os cliques maximais
+def encontrar_cliques_maximais(grafo):      # Função para encontrar os cliques maximais utilizando o Algoritmo de Bron Kerbosch
+
+    cliques_maximais = []
+    bron_kerbosch(set(), set(grafo.nodes()), set(), cliques_maximais)  
+
+    print("\nCliques Maximais:")   
+
+    for i in range(0, len(cliques_maximais)):
+        print(f"Clique {i + 1} ({len(cliques_maximais[i])} vértices): {cliques_maximais[i]}")
     
-    """
-    Exibir todos os cliques maximais no grafo.
-    grafo (networkx.Graph): Grafo carregado no NetworkX.
-    list: Lista de cliques maximais, onde cada clique é representado por uma lista de nós.
-    """
-
-    cliques_maximais = list(nx.find_cliques(grafo))    # Encontrar os cliques maximais usando NetworkX
-
-    print("\nCliques Maximais:")   # Exibição dos cliques encontrados.
-
-    for i, clique in enumerate(cliques_maximais, start=1):
-        print(f"Clique {i} ({len(clique)} vértices): {clique}")
-
     return cliques_maximais
 
-MAX_NODOS = -1        # Variável global para o número máximo de nós no grafo.
-matriz = mmread("soc-dolphins\\soc-dolphins.mtx")    # Carrega a matriz do arquivo no formato Matrix Market.
-grafo = nx.Graph(matriz)     # Construção do grafo usando NetworkX.
-MAX_NODOS = len(grafo.nodes())    # Definição do número total de nós.
+
+
+matriz = mmread("soc-dolphins\\soc-dolphins.mtx")    # Carrega a matriz do arquivo.
+
+grafo = nx.Graph(matriz)                    # Construção do grafo usando NetworkX.
+
+MAX_NODOS = len(grafo.nodes())              # Definição do número total de nós.
 
 lista_de_adj = inicializar_lista_adj(grafo) # Construção da Lista de adjacências.
+
 tabela_graus_por_vertice = grau_por_vertice(lista_de_adj)    # Cálculo dos graus de cada vértice.
 
 if __name__ == "__main__":
-    
     menu() 
 
 
